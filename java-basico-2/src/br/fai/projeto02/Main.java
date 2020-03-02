ece529469a6d293008dfc795a0cfd6c33efe95a4
Matuk <mateusmatuk@fai-mg.br>
2020-03-02 20:21:43 -0300

    primeiro commit

---
+++ java-basico-2/src/br/fai/projeto02/Main.java
@@ -0,0 +1,17 @@
+package br.fai.projeto02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main app = new Main();
+		app.start();
+		
+	}
+
+	private void start() {
+		
+		
+	}
+
+}
